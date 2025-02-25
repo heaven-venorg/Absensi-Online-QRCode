54b490d4461cab5d62c211488ff00eafae60a2c0
heaven.venorg <heavensipangkar1@gmail.com>
2025-02-25 11:07:09 +0700

    update alert success and alert danger

--- app/Http/Controllers/AbsencesController.php
+++ app/Http/Controllers/AbsencesController.php
@@ -24,15 +24,19 @@ class AbsencesController extends Controller
         $absensiToday = Absences::where('name', $user->name)->whereDate('absensi_time', $today)->first();
         if ($absensiToday) {
             return redirect()->route('root')->with('alert', 'Anda sudah melakukan absensi');
-        } elseif (Carbon::now()->hour >= 06) {
+        } elseif (Carbon::now()->hour >= 12) {
             return redirect()->route('root')->with('alert', 'Anda Terlambat, dan tidak bisa absensi');
+        } elseif (Carbon::now()->hour >= 12 && Carbon::now()->dayOfWeek === 2) {
+            return redirect()->route('root')->with('alert', 'Anda Terlambat, dan tidak bisa absensi');
+        } elseif (Carbon::now()->dayOfWeek === 0 || Carbon::now()->dayOfWeek === 6) {
+            return redirect()->route('root')->with('alert', 'Hari ini libur, tidak bisa absensi');
         } else {
             Absences::create([
                 'user_id' => $user->id,
                 'name' => $user->name,
                 'absensi_time' => Carbon::now()
             ]);
-            return redirect()->route('root')->with('alert', 'Absensi anda telah direkam');
+            return redirect()->route('root')->with('success', 'Absensi anda telah direkam');
         }
     }
 }
--- resources/views/page/root/index.blade.php
+++ resources/views/page/root/index.blade.php
@@ -4,6 +4,9 @@
         @if (session('alert'))
             <div class="alert alert-danger text-capitalize">{{ session('alert') }}</div>
         @endif
+        @if (session('success'))
+            <div class="alert alert-success text-capitalize">{{ session('success') }}</div>
+        @endif
         <img src="{{ asset('assets/profile.png') }}" alt="Profile" class="rounded-circle border border-3" width="100px">
         <h2 class="fw-bolder text-capitalize h2">Hello {{ Auth::user()->name }}</h2>
         <a href="{{ route('absences.view') }}" class="btn btn-success w-25 text-capitalize fw-bold">Absensi disini</a>
